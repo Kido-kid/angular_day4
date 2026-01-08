b0ebf20f4697bd65e127f7385f42494f32d2df6c
Kido-kid <akshaygit.git@gmail.com>
2026-01-08 17:08:11 +0530

    Initial message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+
+interface User{
+  name:string;
+  Role: "Teacher" | "Parent" | "Student";
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
 export class AppComponent {
   title = 'UserManagementSystem';
+  roles: Array<"Teacher" | "Parent" | "Student"> = ['Teacher', 'Parent', 'Student'];
+  selectedRole: "Teacher" | "Parent" | "Student" = this.roles[0];
+  message: string = '';
+
+  user:User={
+    name:'',
+    Role: this.selectedRole
+  }
+
+ get isNameEmpty(): boolean{
+  return this.user.name.trim()==='';
+ }
+
+  users: User[] = [];
+
+  addUser(){
+    if(this.user.name.trim()){
+      try{
+      this.users.push({...this.user});
+      this.message=`User Name : "${this.user.name}" & Role : "${this.user.Role}" - added successfully`;
+            setTimeout(() => {
+        this.message = '';
+      },5000);
+
+      }catch(e){
+        console.log(Error);
+        this.message =  ` Cannot able to add User Name : ${this.user.name} & Role : ${this.user.Role} `;
+              setTimeout(() => {
+        this.message = '';
+      }, 5000);
+
+      }
+      this.user.name = '';
+      // this.user.Role = this.selectedRole;
+      
+    }
+    
+
+  }
+
+  getUsersByRole(role: "Teacher" | "Parent" | "Student") {
+  return this.users.filter(u => u.Role === role);
+}
+
+
 }
